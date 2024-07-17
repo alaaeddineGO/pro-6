debc9162130576a100e33d8dcb945aa2cc3543d1
unknown <alaago112000@gmail.com>
2024-07-17 13:01:25 +0200

    v.1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Cards from './components/Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Reliable, efficient delivery</h2>
+      <h1>Powered by Technology</h1>
+      <p> 
+        Our Artificial Intelligence powered tools use millions of project data <br/>
+        points to ensure that your project is successful
+      </p>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
